c8501efe8b02b52965906b323e069cd269982ab0
HadesJK <jinqiliang2007@163.com>
2016-03-24 17:11:46 +0800

    files upload

--- src/main/java/cn/edu/zju/isee/cms/controller/CTFileController.java
+++ src/main/java/cn/edu/zju/isee/cms/controller/CTFileController.java
@@ -1,10 +1,13 @@
 package cn.edu.zju.isee.cms.controller;
 
+import cn.edu.zju.isee.cms.utils.file.FileUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
-import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
 
 /**
  * Created by jql on 2016/3/24.
@@ -13,14 +16,23 @@ import javax.servlet.http.HttpServletRequest;
 public class CTFileController {
 
     @RequestMapping("upload")
-     public String fileUpload() {
+    public String fileUpload() {
         return "upload/upload";
     }
 
     @RequestMapping("filesUpload")
-    public String upload(HttpServletRequest request, Model model) {
-        String fileName = request.getParameter("fileName");
-        model.addAttribute("fileName", fileName);
+    public String upload(@RequestParam("files") MultipartFile[] files, Model model) {
+        if (files != null) {
+            for (MultipartFile file : files) {
+                String name = file.getOriginalFilename();
+                try {
+                    FileUtils.saveFile(file.getInputStream(), name);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        model.addAttribute("fileName", "success");
         return "upload/uploadSuccess";
     }
 }
--- src/main/java/cn/edu/zju/isee/cms/utils/file/FileUtils.java
+++ src/main/java/cn/edu/zju/isee/cms/utils/file/FileUtils.java
@@ -1,9 +1,16 @@
 package cn.edu.zju.isee.cms.utils.file;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+
 /**
  * Created by jql on 2016/3/24.
  */
 public class FileUtils {
-    public static final String BASE_PATH = "";
+    private static final String BASE_PATH = "/home/jql/";
 
+    public static void saveFile(InputStream in, String name) throws IOException {
+        org.apache.commons.io.FileUtils.copyInputStreamToFile(in, new File(BASE_PATH + name));
+    }
 }
