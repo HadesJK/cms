0c2929e9b17fd5de91b1607224106f5ddf35c08d
HadesJK <jinqiliang2007@163.com>
2016-03-25 19:25:17 +0800

    modified configurations xml

--- src/main/java/cn/edu/zju/isee/cms/Main.java
+++ src/main/java/cn/edu/zju/isee/cms/Main.java
@@ -9,7 +9,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
  */
 public class Main {
     public static void main(String[] args) {
-        ApplicationContext context = new ClassPathXmlApplicationContext("springMVC-servlet.xml");
+        ApplicationContext context = new ClassPathXmlApplicationContext("springmvc/springMVC-servlet.xml");
         if (context == null) {
             System.out.println("impossible.");
         } else {
