095d4a63b348aac01f3539d5a5843f6309cb0a29
NecessaryEvil <943038365@qq.com>
2016-03-29 18:49:28 +0800

    mch download

--- src/main/java/cn/edu/zju/isee/cms/controller/CTFileController.java
+++ src/main/java/cn/edu/zju/isee/cms/controller/CTFileController.java
@@ -1,14 +1,20 @@
 package cn.edu.zju.isee.cms.controller;
 
+import cn.edu.zju.isee.cms.GlobalConstant;
 import cn.edu.zju.isee.cms.service.LungImgSlideService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.FileInputStream;
 
 /**
  * Created by jql on 2016/3/24.
@@ -32,5 +38,28 @@ public class CTFileController {
             service.saveFiles(files);
         }
         return "husky";
+    }
+
+    @RequestMapping(value = "/list/Download", method = RequestMethod.GET)
+    public void handleFileDownload(HttpServletResponse res, @RequestParam("id") String id) {
+        String filename = "SL0058.zip";
+        try {
+//            String fn = "/Test.xls";
+//            URL url = getClass().getResource(fn);
+//            File f = new File(url.toURI());
+            System.out.println(id);
+            File f = new File(GlobalConstant.ZIP_ZHEYI_LUNG + "/1/" + filename);
+            System.out.println("Loading file "+ filename +"("+f.getAbsolutePath()+")");
+            if (f.exists()) {
+                res.setContentType("application/zip");
+                res.setContentLength(new Long(f.length()).intValue());
+                res.setHeader("Content-Disposition", "attachment; filename="+f.getName());
+                FileCopyUtils.copy(new FileInputStream(f), res.getOutputStream());
+            } else {
+                System.out.println("File"+ filename +"("+f.getAbsolutePath()+") does not exist");
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
