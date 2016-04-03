3b49383e78cb2e712fa4509a1f4ef671dbc8713d
ZL <zero2011zl@163.com>
2016-04-03 16:35:16 +0800

    dicom image view

--- src/main/java/cn/edu/zju/isee/cms/controller/CTController.java
+++ src/main/java/cn/edu/zju/isee/cms/controller/CTController.java
@@ -1,10 +1,9 @@
 package cn.edu.zju.isee.cms.controller;
 
+import cn.edu.zju.isee.cms.GlobalConstant;
 import cn.edu.zju.isee.cms.controller.model.RecogModel;
 import cn.edu.zju.isee.cms.entity.CT;
 import cn.edu.zju.isee.cms.entity.CTSlide;
-import cn.edu.zju.isee.cms.mapper.CTMapper;
-import cn.edu.zju.isee.cms.mapper.CTSlideMapper;
 import cn.edu.zju.isee.cms.service.LungCTImgService;
 import cn.edu.zju.isee.cms.service.LungImgSlideService;
 import org.apache.velocity.tools.generic.DateTool;
@@ -14,8 +13,11 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.OutputStream;
 import java.util.List;
-import java.util.Map;
 
 /**
  * Created by jql on 2016/3/24.
@@ -56,6 +58,16 @@ public class CTController {
         String msg;
         try {
             List<RecogModel> result = imgService.ctPredict(ctId);
+
+//            List<RecogModel> result = new ArrayList<RecogModel>();
+//            for(int i =1; i < 6; i++) {
+//                RecogModel rModel = new RecogModel();
+//                rModel.pic = "sorry @mch";
+//                rModel.label = "test";
+//                rModel.probably = "cloudy day";
+//                rModel.serialNum = i;
+//                result.add(rModel);
+//            }
             model.addAttribute("preds", result);
             msg = "ctlist/predList";
         } catch (Exception e) {
@@ -66,6 +78,35 @@ public class CTController {
         return msg;
     }
 
+    @RequestMapping("/dicomView/{slideId}")
+    public String ctDicomView(@PathVariable int slideId, Model model){
+        model.addAttribute("dicomName", slideId + ".dcm");
+        model.addAttribute("dicomUrl", "/dicomPath/" + slideId);
+//        model.addAttribute("dicomUrl", "https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=2413511449,3739465490&fm=116&gp=0.jpg");//GlobalConstant.JAVA_MATLAB_DIR + slideId + ".dcm");
+        return "ctlist/dicomView";
+    }
 
+    @RequestMapping("/dicomPath/{dicomId}")
+    public void getDicom(@PathVariable int dicomId, HttpServletResponse response) {
+        String fileName = GlobalConstant.JAVA_MATLAB_DIR + dicomId + ".dcm";
+        File file = new File(fileName);
+        FileInputStream in = null;
+        try {
+            in = new FileInputStream(file);
+            OutputStream o = response.getOutputStream();
+            int l = 0;
+            byte[] buffer = new byte[4096];
+            while((l = in.read(buffer)) != -1){
+                o.write(buffer,0,l);
+            }
+            o.flush();
+            in.close();
+            o.close();
+        } catch (java.io.IOException e) {
+            e.printStackTrace();
+        }
+
+        return;
+    }
 
 }
